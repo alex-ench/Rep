eada3cff19a29ee17db84ffc924a9bff7f06e096
SHISEN <alex-ench@yandex.ru>
2020-05-28 17:31:52 +0400

    commit

--- Repi.php
+++ Repi.php
@@ -4,3 +4,13 @@ echo 2; echo PHP_EOL;
 echo 3; echo PHP_EOL;
 echo 4; echo PHP_EOL;
 echo 5; echo PHP_EOL;
+
+for ($i = 0; $i < 10; $i++)
+{
+    echo "iteration {$i}".PHP_EOL;
+}
+
+for ($i = 0; $i < 10; $i++)
+{
+    echo 'infinite operation'. PHP_EOL;
+}
