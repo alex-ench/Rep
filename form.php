da60f9c39f9dc2409686dd4dcc8cb30b5329c168
SHISEN <alex-ench@yandex.ru>
2020-06-03 23:56:45 +0400

    commit

--- form.php
+++ form.php
@@ -5,42 +5,63 @@
     <label for="filer">Добавьте файл: </label>
     <input type="file" name="filer"> <br>
 
-    <label for="checkbox[values][1]">Текст </label>
-    <input type="checkbox" name="checkbox[values][1]" value="1"> <br>
-
-    <label for="checkbox[values][2]">Файл </label>
-    <input type="checkbox" name="checkbox[values][2]" value="2"> <br>
-
     <input type="submit" name="submiter"> <br>
 </form>
 
 <?php
-if(isset($_POST['submiter']))
-{
-    touch('text1.txt');
+if (isset($_POST)) {
+    $file_name = uniqid(rand()) . '.csv';
     $input = $_POST['input'];
-    $f = fopen('text1.txt', 'w');
-    fputs($f, $input);
-
-    $text = file_get_contents('text1.txt');
-    echo $text. PHP_EOL;
-    $text1 = mb_convert_case($text, MB_CASE_TITLE);
-    $count = count(preg_split('/\s+/u', $text, null, PREG_SPLIT_NO_EMPTY)). PHP_EOL;
-    echo '<br>Количество слов в тексте: ', $count, '<br>';
-    function countAndSort($string)
-    {
-        preg_match_all("/[a-zа-я]+/ium", $string, $words);
-
-        $counts = array_count_values(array_map('ucwords', $words[0]));
-        $words = array_keys($counts);
-
-        array_multisort($counts, SORT_NUMERIC, SORT_DESC, $words, SORT_STRING);
-        return array_map(function($a, $b)
+    $filer = $_POST['filer'];
+    if (!empty($input)) {
+        echo $input . PHP_EOL;
+        $text = mb_convert_case($input, MB_CASE_TITLE);
+        $count = count(preg_split('/\s+/u', $input, null, PREG_SPLIT_NO_EMPTY)) . PHP_EOL;
+        echo '<br>Количество слов в тексте: ', $count, '<br>';
+        function countAndSort($string)
         {
-            return "'$a' встречается $b раз <br>". PHP_EOL;
-        }, $words, $counts);
+            preg_match_all("/[a-zа-я]+/ium", $string, $words);
+            $counts = array_count_values(array_map('ucwords', $words[0]));
+            $words = array_keys($counts);
+            array_multisort($counts, SORT_NUMERIC, SORT_DESC, $words, SORT_STRING);
+            return array_map(function ($a, $b) {
+                return "'$a' встречается $b раз";
+            }, $words, $counts);
+        }
+
+        $wordi = countAndSort($text);
+        print_r($wordi);
+
+        $fp = fopen("Results/$file_name", 'w');
+        foreach ($wordi as $fields) {
+            fputcsv($fp, array($fields));
+        }
+        fclose($fp);
+    } elseif (!empty($filer)) {
+        echo $filer . PHP_EOL;
+        $text1 = mb_convert_case($filer, MB_CASE_TITLE);
+        $count1 = count(preg_split('/\s+/u', $filer, null, PREG_SPLIT_NO_EMPTY)) . PHP_EOL;
+        echo '<br>Количество слов в тексте: ', $count1, '<br>';
+        function countAndSort($string)
+        {
+            preg_match_all("/[a-zа-я]+/ium", $string, $words);
+            $counts = array_count_values(array_map('ucwords', $words[0]));
+            $words = array_keys($counts);
+            array_multisort($counts, SORT_NUMERIC, SORT_DESC, $words, SORT_STRING);
+            return array_map(function ($a, $b) {
+                return "'$a' встречается $b раз";
+            }, $words, $counts);
+        }
+
+        $wordi = countAndSort($text1);
+        print_r($wordi);
+
+        $fp = fopen("Results/$file_name", 'w');
+        foreach ($wordi as $fields) {
+            fputcsv($fp, array($fields));
+        }
+        fclose($fp);
+    } else {
+        echo 'Вы ничего не ввели';
     }
-    print_r(countAndSort($text1));
-    fclose($f);
-    unlink('text1.txt');
 }
